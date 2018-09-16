19229ade273aca6a10d299d31da0b1ebd1c4022a
Daniel Da-tze Lee <ddlee929@Daniels-MacBook-Air.local>
2018-09-16 12:51:09 -0500

    fixed url again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://dl-devcamp-portfolio.herokuapp.com', 'http://devcamp-portfolio.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://dl-devcamp-portfolio.herokuapp.com', 'http://dl-devcamp-portfolio.herokuapp.com']
   config.action_cable.url = "wss://dl-devcamp-portfolio.herokuapp.com/cable"
 end
